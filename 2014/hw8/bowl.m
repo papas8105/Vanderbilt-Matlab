e3788b0965f596c2252ebeb9255af3c7077475dc
George Papadopoulos <pgeorgios8@gmail.com>
2017-10-03 21:20:07 +0300

    final bowl
    f

--- 2014/hw8/bowl.m
+++ 2014/hw8/bowl.m
@@ -5,13 +5,13 @@ if ~isvector(v) || size(v,1) ~= 1 || length(v) < 12 || length(v) > 21 || sum(v <
 end
 score  = 0;
 frames = 1;
-ii    = 1 ;
+ii     = 1;
 while frames < 10
     a = v(ii)  ;
     b = v(ii+1);
     c = v(ii+2);
     if a == 10                         %strike
-        score = score + a + b + c;
+        score = score + a + b + c    ;
     else
         if a<10 && a + b == 10         %spare
             score = score + a + b + c;
@@ -24,10 +24,10 @@ while frames < 10
         ii = ii + 1      ;
     end
     frames = frames + 1  ;
-    ii     = ii + 1      ;
+    ii     = ii     + 1  ;
 end
 if length(v) ~= ii + 1 && length(v) ~= ii + 2
-    score = -1;
+    score = -1           ;
     return
 end
 a = v(ii)  ;
@@ -41,7 +41,7 @@ if a + b >= 10
     score = score + a + b + c;
 else
     if length(v) ~= ii + 1
-        score = -1;
+        score = -1           ;
         return
     end
     score = score + a + b    ;
