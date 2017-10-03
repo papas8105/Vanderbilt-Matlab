bb5096dcfee136b5ffbf148d757fa452c6c77fc2
George Papadopoulos <pgeorgios8@gmail.com>
2017-10-03 20:57:50 +0300

    identation of commas
    comma identation

--- 2014/hw8/bowl.m
+++ 2014/hw8/bowl.m
@@ -5,7 +5,7 @@ if ~isvector(v) || size(v,1) ~= 1 || length(v) < 12 || length(v) > 21 || sum(v <
 end
 score  = 0;
 frames = 1;
-ii    = 1;
+ii    = 1 ;
 while frames < 10
     a = v(ii)  ;
     b = v(ii+1);
@@ -15,13 +15,13 @@ while frames < 10
     else
         if a<10 && a + b == 10         %spare
             score = score + a + b + c;
-        elseif a<10 && a + b < 10     %open
+        elseif a<10 && a + b < 10      %open
             score = score + a + b    ;
         else
-            score = -1;
+            score = -1               ;              
             return
         end
-        ii = ii + 1          ;
+        ii = ii + 1      ;
     end
     frames = frames + 1  ;
     ii     = ii + 1      ;
@@ -34,16 +34,16 @@ a = v(ii)  ;
 b = v(ii+1);
 if a + b >= 10
     if length(v) ~= ii + 2
-        score = -1;
+        score = -1           ;
         return
     end
-    c = v(ii+2);
+    c = v(ii+2)              ;
     score = score + a + b + c;
 else
     if length(v) ~= ii + 1
         score = -1;
         return
     end
-    score = score + a + b;
+    score = score + a + b    ;
 end
 end
