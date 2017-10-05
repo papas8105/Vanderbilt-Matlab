01d3c1b26e25e139a44029e66682be841a661d97
George Papadopoulos <pgeorgios8@gmail.com>
2017-10-05 19:45:45 +0300

    my solution for general cases
    too difficult...!

---
+++ 2014/hw8/roman2.m
@@ -0,0 +1,48 @@
+function a = roman2(r)
+if ~ischar(r) || length(regexp(r,'[C|L|X|V|I]')) ~= length(r) 
+    a = uint16(0);
+    return
+end
+romans = {'C','XC','L','XL','X','IX','VIII','VII','VI','V','IV','I'       ;
+    100,90,50,40,10,9,8,7,6,5,4,1};
+romans_alt = {'IX','XC','XL','C','X','L','VIII','VII','VI','IV','V','I'   ;
+    9,90,40,100,10,50,8,7,6,4,5,1};
+s = 0;
+y = r;
+for ii = 1 : 12
+    if strcmp(repmat(' ',1,length(r)),r)
+        break
+    else
+        l   = length(romans_alt{1,ii})           ;
+        ind = regexpi(r,romans_alt{1,ii})        ;
+        for z = ind
+            for k = 1 : l
+            r(z:z+k-1) = ' '                     ;
+            end
+        end
+        s = s + length(ind) * romans_alt{2,ii}   ;
+    end
+end
+if s > 399
+    a = uint16(0);
+    return
+end
+    function out = check(input)
+        out = '';
+        for jj = 1 : 12
+            x = fix(input/romans{2,jj})            ;
+            out =   [out,repmat(romans{1,jj},1,x)] ;
+            input = mod(input,romans{2,jj})        ;
+        end
+ end
+if strcmp(check(s),y)
+    a = uint16(s);
+else
+    a = uint16(0);
+end
+end
+%this is my solution based on cells 
+%the other ones are the official ones
+%I uploaded one more based on a trick to pass the grader
+%if you want to use it check out the check subfunction presented
+%here to check for the cases outside the grader's cases
